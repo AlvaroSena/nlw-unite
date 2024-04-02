8225719c765c944c8e676724f81c00407f5a4920
alvarosena <alvarosenacs.c@gmail.com>
2024-04-02 19:18:08 -0300

    add get event endpoint

---
+++ server/src/routes/get-event.ts
@@ -0,0 +1,62 @@
+import { FastifyInstance } from 'fastify'
+import { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function getEvent(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/events/:eventId',
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        response: {
+          200: {
+            event: z.object({
+              title: z.string(),
+              details: z.string().nullable(),
+              slug: z.string(),
+              maximumAttendees: z.number().int().nullable(),
+              attendeesAmount: z.number().int().nullable(),
+            }),
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params
+
+      const event = await prisma.event.findUnique({
+        select: {
+          title: true,
+          slug: true,
+          details: true,
+          maximumAttendees: true,
+          _count: {
+            select: {
+              attendees: true,
+            },
+          },
+        },
+        where: {
+          id: eventId,
+        },
+      })
+
+      if (!event) {
+        throw new Error('Event not found.')
+      }
+
+      return reply.send({
+        event: {
+          title: event.title,
+          details: event.details,
+          slug: event.slug,
+          maximumAttendees: event.maximumAttendees,
+          attendeesAmount: event._count.attendees,
+        },
+      })
+    },
+  )
+}
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,12 +5,14 @@ import {
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
 import { registerForEvent } from './routes/register-for-event'
+import { getEvent } from './routes/get-event'
 
 const app = fastify()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
 app.register(registerForEvent)
+app.register(getEvent)
 
 app
   .listen({
