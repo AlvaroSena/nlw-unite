fd64ee83c064a9aaa75cefc9ebbd8bd893a9977a
alvarosena <alvarosenacs.c@gmail.com>
2024-04-02 19:06:27 -0300

    add register for an event endpoint

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,11 +4,13 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { createEvent } from './routes/create-event'
+import { registerForEvent } from './routes/register-for-event'
 
 const app = fastify()
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 app.register(createEvent)
+app.register(registerForEvent)
 
 app
   .listen({
