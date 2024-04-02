ce70377c9f10047f4618e983448d7d73763e108e
alvarosena <alvarosenacs.c@gmail.com>
2024-04-02 20:52:58 -0300

    add header and attedee-list components

--- web/src/components/header.tsx
+++ web/src/components/header.tsx
@@ -1,7 +1,18 @@
+import nlwUniteIcon from '../assets/nlw-unite-icon.svg'
+
 export function Header() {
   return (
-    <header className="">
-      <div></div>
-    </header>
+    <div className="flex items-center gap-5 py-2">
+      <img src={nlwUniteIcon} alt="NLW Unite Icon" />
+
+      <nav className="flex items-center gap-5">
+        <a className="font-medium text-sm text-zinc-300" href="">
+          Eventos
+        </a>
+        <a className="font-medium text-sm" href="">
+          Participantes
+        </a>
+      </nav>
+    </div>
   )
 }
