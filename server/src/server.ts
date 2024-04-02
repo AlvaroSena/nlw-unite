0c85c569a08d2f6a56a10c476d29dfeb0968fce2
alvarosena <alvarosenacs.c@gmail.com>
2024-04-02 18:09:09 -0300

    create event

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,31 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+const prisma = new PrismaClient()
 
-app.get('/', () => {
-  return 'hello'
+app.post('/events', async (request, reply) => {
+  const createEventSchema = z.object({
+    title: z.string().min(4),
+    details: z.string().nullable(),
+    maximumAttendees: z.number().int().positive().nullable(),
+  })
+
+  const { title, details, maximumAttendees } = createEventSchema.parse(
+    request.body,
+  )
+
+  const event = await prisma.event.create({
+    data: {
+      title,
+      details,
+      maximumAttendees,
+      slug: new Date().toISOString(),
+    },
+  })
+
+  return reply.status(201).send({ eventId: event.id })
 })
 
 app
